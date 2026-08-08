2f0b14bdb5e639a8ea7ddc386f019cbcdc2db057
Yihan <ehan.cii66@gmail.com>
2026-08-08 21:31:27 +0800

    修复鸿蒙折射着色器黑屏

--- refraction/entry/src/main/cpp/shader/shader_refraction.h
+++ refraction/entry/src/main/cpp/shader/shader_refraction.h
@@ -7,7 +7,9 @@
 #ifndef REFRACTIONMODEL_SHADER_REFRACTION_H
 #define REFRACTIONMODEL_SHADER_REFRACTION_H
 
-const char *refractionVertexShader = R"(#version 300 es\nprecision highp float;\nprecision highp int;
+const char *refractionVertexShader = R"(#version 300 es
+precision highp float;
+precision highp int;
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aNormal;
 layout(location = 2) in vec2 aTexCoord;
@@ -110,7 +112,9 @@ void main() {
 }
 )";
 
-const char *refractionFragmentShader = R"(#version 300 es\nprecision highp float;\nprecision highp int;
+const char *refractionFragmentShader = R"(#version 300 es
+precision highp float;
+precision highp int;
 
 uniform float uFresnelPower;
 uniform float uShininess;
